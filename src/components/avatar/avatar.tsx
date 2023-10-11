0f248b7f90068ea5e9331f14aa05b283fd3aacba
Dulaj Kavinda <dulajkaavinda@gmail.com>
2023-10-11 19:52:21 +0530

    fix: change resource icon color

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -7,7 +7,6 @@ export type AvatarProps = {
   size?: AvatarSize;
   className?: string;
   customStyle?: React.CSSProperties;
-  alt?: string;
   ariaLabel?: string | undefined;
 };
 
@@ -30,7 +29,7 @@ export type AvatarSize =
   | "30";
 
 const Avatar: React.FC<AvatarProps> = (props) => {
-  const { size, shape, img, className, customStyle, ariaLabel, alt } = props;
+  const { size, shape, img, className, customStyle, ariaLabel } = props;
 
   const styles = classnames.default(
     "folio-avatar",
@@ -57,7 +56,6 @@ Avatar.defaultProps = {
   shape: "circle",
   className: "",
   customStyle: {},
-  alt: undefined,
   ariaLabel: undefined,
 };
 
